55a5fac0f8a64331e11b1dbcc65c55d155af9fb7
Ahmad Parvaresh <ahmad.parvaresh@globallogic.com>
2024-06-03 18:17:21 +0200

    ASAP-5 Updating the project configuration to build all the exercises

--- Exercise_3/main.cpp
+++ Exercise_3/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
-void unique_numbers( int[], const unsigned int);
+// void unique_numbers( int[], const unsigned int);
+void unique_numbers( int /*array*/[] , const unsigned int /*col_size*/);
 
 int main (){
 
     // calling the function
 
     int nums [] {12,3,21,3,8,2,21,4,3};
-    const unsigned int col_size = 9; 
-    unique_numbers(nums,col_size);
+    
+    unique_numbers(nums,std::size(nums));
 
     // expected result: 
     // The collection contains 6 unique numbers, they are : 12 3 21 8 2 4 
@@ -16,7 +17,7 @@ int main (){
     return 0;
 }
 
-void unique_numbers( int[], const unsigned int){
+void unique_numbers( int numbers[], const unsigned int collection_size){
     //Write your code here
 
 }
